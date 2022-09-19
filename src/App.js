68a34d4dcc7b825299b2211b544be4fb9931f619
lucasanand9 <lucasanand9@gmail.com>
2022-09-18 21:18:17 -0300

    Projeto front-end para uma rede social, pagina de login sendo criada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  <div>
+    <div className='Capi'>
+{/* mostrar assim pro grupe e depois colocar alguma coisa parecida com o instagram */}
+
     </div>
+<form autocomplete="off">
+    <Box  
+    display='flex'
+    flexDirection={'column'}
+    spacing={2}
+    
+    >
+        <div className='titulo'>
+        <Typography variant="h3">Login</Typography>
+        </div>
+        <hr></hr>
+        <div className='input-email'>
+            <TextField id="Email" label="Email" variant="outlined" autocomplete = "off"/>
+        </div>
+        <div className='input-senha'>
+            <TextField id="Senha"label="Senha" variant='outlined' type={'password'}/>
+        </div>
+        <div className='botao'>
+            <Button variant="contained" id="Login">Login</Button>
+            <Button>Esqueci a Senha</Button>
+        </div>
+        <div className='rodape'>
+           <p>Caso não tenha uma conta <a href="">registre-se</a></p>
+        </div>
+    </Box>
+</form>
+</div>
+    
   );
 }
 
